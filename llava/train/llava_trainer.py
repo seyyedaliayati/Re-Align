b5ad0e4c405aaaed41f78c6ada2577dda53fa5da
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2025-04-22 13:59:42 -0500

    refactor: remove metrics argument from _save_checkpoint calls in LLaVATrainer and rDPOTrainer; update sft_weight in run.slurm

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -271,7 +271,7 @@ class LLaVATrainer(DPOTrainer):
                 self.model.config.save_pretrained(output_dir)
                 torch.save(weight_to_save, os.path.join(output_dir, f'mm_projector.bin'))
         else:
-            super(LLaVATrainer, self)._save_checkpoint(model, trial, metrics)
+            super(LLaVATrainer, self)._save_checkpoint(model, trial)
 
     def _save(self, output_dir: Optional[str] = None, state_dict=None):
         if getattr(self.args, 'tune_mm_mlp_adapter', False):
@@ -397,7 +397,7 @@ class LLaVArDPOTrainer(rDPOTrainer):
                 self.model.config.save_pretrained(output_dir)
                 torch.save(weight_to_save, os.path.join(output_dir, f'mm_projector.bin'))
         else:
-            super(LLaVArDPOTrainer, self)._save_checkpoint(model, trial, metrics)
+            super(LLaVArDPOTrainer, self)._save_checkpoint(model, trial)
 
     def _save(self, output_dir: Optional[str] = None, state_dict=None):
         if getattr(self.args, 'tune_mm_mlp_adapter', False):
